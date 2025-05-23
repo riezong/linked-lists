4a5ef1457255cdc18b6631c28cb54fde8488e242
Andrew <71823699+riezong@users.noreply.github.com>
2025-05-23 14:54:48 +1000

    Create LinkedList and Node factories

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const LinkedList = function () {
+  // represent the full list
+
+  function append(value) {}
+  function prepend(value) {}
+  function size() {}
+  function head() {}
+  function at(index) {}
+  function pop() {}
+  function contains(value) {}
+  function find(value) {}
+  function toString() {}
+
+  // Extra credit
+  function insertAt(value, index) {}
+  function removeAt(index) {}
+};
+
+const Node = function () {
+  // value and nextNode set to null by default
+  this.value = null;
+  this.nextNode = null;
+};
